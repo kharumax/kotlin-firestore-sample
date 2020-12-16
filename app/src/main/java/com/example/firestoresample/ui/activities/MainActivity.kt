870d8c63764588d2ea79849b22d30026d687c18c
kharumax <kharumax@gmail.com>
2020-12-16 17:56:19 +0900

    Navigation Settings

--- app/src/main/java/com/example/firestoresample/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/firestoresample/ui/activities/MainActivity.kt
@@ -2,12 +2,33 @@ package com.example.firestoresample.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.firestoresample.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostHomeFragment)
+        val appBarConfiguration =  AppBarConfiguration(
+            setOf(R.id.feedFragment,R.id.searchFragment,R.id.profileFragment)
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
